df236ff974570be7ea00c735dec1ef523bd16631
Georges Sich <danzels5000@gmail.com>
2021-01-25 00:30:01 +0300

    start responsive (add burger)

--- js/accordeon.js
+++ js/accordeon.js
@@ -3,4 +3,3 @@ $('.tab-title').on('click', function() {
     $('.tab-title .panel').not($(this).next()).slideUp(250);
     $(this).next().slideToggle(500);
 });
-
--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 $(function() {
     $('.slider').slick ({
         infinite: true,
+        arrows: true,
+        dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: true,
-        dots: false
+        responsive: [
+             {
+                breakpoint: 1210,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 792,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
     });
+});
+
+$('.menu__btn').on('click', function(){
+    $('.header-menu__list').toggleClass('header-menu__list--active');
 });
